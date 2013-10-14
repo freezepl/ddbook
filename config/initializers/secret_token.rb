2e18e609a7f7d7655dfab80fb01a4251977fc3d1
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2013-10-14 21:56:23 +0200

    initial commit with devise and mysql2 in Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Desdev::Application.config.secret_key_base = '83bd5d31a29921e5746d14ed3adb263aa51bce9d98dcb83ba451990a760a67e57050795adb2773bb93b5995fea83de66cb43682822302b597f452aeedb82a0bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Desdev::Application.config.session_store :cookie_store, key: '_desdev_session'
